43f32f12aec2c0e8d700ffde7d63adc790ce5565
Muhammad Taufik <heryun@heryun-2.local>
2023-09-13 22:04:54 +0700

    update caraousel

--- src/component/Navigation.jsx
+++ src/component/Navigation.jsx
@@ -1,8 +1,19 @@
 import React, {useState} from "react";
-import { AiFillHome, AiOutlineSearch, AiOutlinePlusSquare, AiOutlineMessage } from "react-icons/ai";
-import {FaPaperPlane} from "react-icons/fa6";
+import { AiFillHome, AiOutlineSearch, AiOutlinePlusSquare, AiOutlineMessage, AiOutlineUserAdd } from "react-icons/ai";
+import {FaPaperPlane, } from "react-icons/fa6";
 import { Link } from "react-router-dom";
-const Navigation = () => {
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+import { EffectCoverflow, Pagination, Navigation } from 'swiper';
+const NavigationButton = () => {
+    const [showModal, setShowModal] = React.useState(false);
+    const slide_image = 'https://cutewallpaper.org/24/image-placeholder-png/index-of-img.png';
+    
     return (
         <>
         
@@ -16,6 +27,119 @@ const Navigation = () => {
                 <Link to="/chat" type="button" className="inline-flex flex-col items-center justify-center px-5 group">
                     <FaPaperPlane className="w-8 h-8 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500 p-1" />
                 </Link>
+                <button
+        className="inline-flex flex-col items-center justify-center px-5 group"
+        type="button"
+        onClick={() => setShowModal(true)}
+      >
+        <AiOutlineUserAdd className="w-8 h-8 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500 p-1" />
+      </button> 
+      {showModal ? (
+        <>
+          <div
+            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+          >
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                  <h3 className="text-3xl font-semibold">
+                    Modal Title
+                  </h3>
+                  <button
+                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    onClick={() => setShowModal(false)}
+                  >
+                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                      ×
+                    </span>
+                  </button>
+                </div>
+                {/*body*/}
+                <div className="relative p-6 flex-auto">
+                <Swiper
+        effect={'coverflow'}
+        grabCursor={true}
+        centeredSlides={true}
+        loop={true}
+        slidesPerView={'auto'}
+        coverflowEffect={{
+          rotate: 0,
+          stretch: 0,
+          depth: 100,
+          modifier: 2.5,
+        }}
+        pagination={{ el: '.swiper-pagination', clickable: true }}
+        navigation={{
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+          clickable: true,
+        }}
+        modules={[EffectCoverflow, Pagination, Navigation]}
+        className="swiper_container"
+      >
+        <SwiperSlide>
+          <img src={slide_image} alt="slide_image" />
+          <div className="slide-description">
+            <p>Taufik Heryunanto</p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image} alt="slide_image" />
+          <div className="slide-description">
+            <p>Heryunanto</p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image} alt="slide_image" />
+          <div className="slide-description">
+            <p>Opik</p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image} alt="slide_image" />
+          <div className="slide-description">
+            <p>jsajjs</p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image} alt="slide_image" />
+        </SwiperSlide>
+
+        <div className="slider-controler">
+          <div className="swiper-button-prev slider-arrow">
+            <ion-icon name="arrow-back-outline"></ion-icon>
+          </div>
+          <div className="swiper-button-next slider-arrow">
+            <ion-icon name="arrow-forward-outline"></ion-icon>
+          </div>
+          <div className="swiper-pagination"></div>
+        </div>
+      </Swiper>
+                </div>
+                {/*footer*/}
+                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                  <button
+                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Close
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
                 </div>
             </div>
         </div>
@@ -44,4 +168,4 @@ const Navigation = () => {
     );
 };
 
-export default Navigation;
+export default NavigationButton;
--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,25 +1,15 @@
 import { motion } from "framer-motion";
-import { Swiper, SwiperSlide } from 'swiper/react';
 import React, { useEffect, useState } from "react";
 import ProfilePostCard from "../component/ProfilePostCard";
 import { AiOutlineSearch as SearchIcon } from "react-icons/ai";
 import Loading from "../component/Loading";
 import { FakeUsers } from "../constants/fakeData";
-import NavigationButton from "../component/Navigation";
-
-import 'swiper/css';
-import 'swiper/css/effect-coverflow';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
-
-import { EffectCoverflow, Pagination, Navigation } from 'swiper';
+import Navigation from "../component/Navigation";
 
 const Explore = () => {
   const [posts, setposts] = useState([]);
   const [limitNum, setLimitNum] = useState(9);
   const [loading, setLoading] = useState(true);
-  const slide_image = 'https://cutewallpaper.org/24/image-placeholder-png/index-of-img.png';
-
   useEffect(() => {
     const getData = async () => {
 
@@ -29,11 +19,9 @@ const Explore = () => {
     };
     getData();
   }, [limitNum]);
-
-
   return (
     <div>
-      <NavigationButton />
+      <Navigation />
       <div className="lg:max-w-4xl mt-14 lg:mx-auto mb-8">
         <div className="block sm:hidden p-2">
           <div className="p-2 items-center w-full border-[1px] rounded">
@@ -48,71 +36,6 @@ const Explore = () => {
               </div>
             </form>
           </div>
-          <Swiper
-        effect={'coverflow'}
-        grabCursor={true}
-        centeredSlides={true}
-        loop={true}
-        slidesPerView={'auto'}
-        coverflowEffect={{
-          rotate: 0,
-          stretch: 0,
-          depth: 100,
-          modifier: 2.5,
-        }}
-        pagination={{ el: '.swiper-pagination', clickable: true }}
-        navigation={{
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-          clickable: true,
-        }}
-        modules={[EffectCoverflow, Pagination, Navigation]}
-        className="swiper_container"
-      >
-        <SwiperSlide>
-          <img src={slide_image} alt="slide_image" />
-          <div className="slide-description">
-            <p>Taufik Heryunanto</p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={slide_image} alt="slide_image" />
-          <div className="slide-description">
-            <p>Heryunanto</p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={slide_image} alt="slide_image" />
-          <div className="slide-description">
-            <p>Opik</p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={slide_image} alt="slide_image" />
-          <div className="slide-description">
-            <p>jsajjs</p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={slide_image} alt="slide_image" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={slide_image} alt="slide_image" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={slide_image} alt="slide_image" />
-        </SwiperSlide>
-
-        <div className="slider-controler">
-          <div className="swiper-button-prev slider-arrow">
-            <ion-icon name="arrow-back-outline"></ion-icon>
-          </div>
-          <div className="swiper-button-next slider-arrow">
-            <ion-icon name="arrow-forward-outline"></ion-icon>
-          </div>
-          <div className="swiper-pagination"></div>
-        </div>
-      </Swiper>
         </div>
         {loading && (
           <div className="flex items-center justify-center h-screen">
